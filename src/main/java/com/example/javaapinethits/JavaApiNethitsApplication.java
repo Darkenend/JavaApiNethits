f0ddd27521337a0c997711bac448947f76ad0fd8
Alvaro Real <34766606+Darkenend@users.noreply.github.com>
2022-04-28 17:16:23 +0200

    Initial Commit

---
+++ src/main/java/com/example/javaapinethits/JavaApiNethitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaapinethits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiNethitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApiNethitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaapinethits/JavaApiNethitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaapinethits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiNethitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
